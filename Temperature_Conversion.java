b39224ccef8d57c357d356a1aa333a3d779a7f1d
LakshmiSnehitha <119840583+LakshmiSnehitha@users.noreply.github.com>
2024-01-08 10:53:17 +0530

    import java.util.*; public class My{     public static void main(String [] args)     {         int a;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         double d=a*(9/5.0);         double c=32+d;         System.out.printf("%.2f",c);     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class My{
+    public static void main(String [] args)
+    {
+        int a;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        double d=a*(9/5.0);
+        double c=32+d;
+        System.out.printf("%.2f",c);
+    }
+}
