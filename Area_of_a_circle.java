e53c11bc246140ff54d84cea50dc19dc830392c5
LakshmiSnehitha <119840583+LakshmiSnehitha@users.noreply.github.com>
2024-01-08 10:40:07 +0530

    import java.util.*; public class My{     public static void main(String [] args)     {         int a;         Scanner sc=new Scanner (System.in);         a=sc.nextInt();         double b=3.14*a*a;         System.out.printf("%.2f",b);     } }

--- Area_of_a_circle.java
+++ Area_of_a_circle.java
@@ -1,11 +1,11 @@
 import java.util.*;
 public class My{
-    public static void main(String[] args)
+    public static void main(String [] args)
     {
         int a;
-        Scanner sc=new Scanner(System.in);
+        Scanner sc=new Scanner (System.in);
         a=sc.nextInt();
-        double c=3.14*a*a;
-        System.out.printf("%.2f",c);
+        double b=3.14*a*a;
+        System.out.printf("%.2f",b);
     }
 }
