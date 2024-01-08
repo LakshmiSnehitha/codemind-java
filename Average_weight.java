b0090c4ab34afe1c611e152159adf0e6a78f3d08
LakshmiSnehitha <119840583+LakshmiSnehitha@users.noreply.github.com>
2024-01-08 11:14:48 +0530

    import java.util.*; public class My{     public static void main(String[] args)     {         int a,b1,b2;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b1=sc.nextInt();         b2=sc.nextInt();         int b3=(3*a)-b1-b2;         System.out.println(b3);     } }

---
+++ Average_weight.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class My{
+    public static void main(String[] args)
+    {
+        int a,b1,b2;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b1=sc.nextInt();
+        b2=sc.nextInt();
+        int b3=(3*a)-b1-b2;
+        System.out.println(b3);
+    }
+}
