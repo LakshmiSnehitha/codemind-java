8791e9ca209eb7f6b56b4bc5c0c1a126e44d7a98
LakshmiSnehitha <119840583+LakshmiSnehitha@users.noreply.github.com>
2024-01-17 20:33:34 +0530

    import java.util.*; public class My{     public static void main(String[] args)     {         int a,b;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt();         int c=a*5+b*7;         System.out.println(c);     } }

---
+++ King_Tours_.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class My{
+    public static void main(String[] args)
+    {
+        int a,b;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        int c=a*5+b*7;
+        System.out.println(c);
+    }
+}
