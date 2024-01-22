7e6f39ab8bcaf371075414ec01ea221ebae608dd
LakshmiSnehitha <119840583+LakshmiSnehitha@users.noreply.github.com>
2024-01-22 09:49:33 +0530

    import java.util.*; public class My{     public static void main(String[] args)     {         int a,b;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt();         int c=b/a;         System.out.println(c);     } }

---
+++ Required_Points:.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class My{
+    public static void main(String[] args)
+    {
+        int a,b;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        int c=b/a;
+        System.out.println(c);
+    }
+}
