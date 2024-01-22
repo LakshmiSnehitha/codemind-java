3e2a6f3ad60db0b68852c19e80d0a3d5c8324b8f
LakshmiSnehitha <119840583+LakshmiSnehitha@users.noreply.github.com>
2024-01-22 10:16:58 +0530

    import java.util.*; public class My{     public static void main(String[] args)     {         int a;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();       double b=2.54*a;         System.out.printf("%.2f",b);     } }

---
+++ Inches_to_Centimeters.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class My{
+    public static void main(String[] args)
+    {
+        int a;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+      double b=2.54*a;
+        System.out.printf("%.2f",b);
+    }
+}
