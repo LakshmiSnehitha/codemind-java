08d5f62d407302abc50d48a33fa58238d17f8f67
LakshmiSnehitha <119840583+LakshmiSnehitha@users.noreply.github.com>
2024-01-22 10:09:44 +0530

    import java.util.*; public class My{     public static void main(String[] args)     {         int a;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();       double b=a*(5.0/18.0);         System.out.printf("%.2f",b);     } }

---
+++ kmph_to_mps.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class My{
+    public static void main(String[] args)
+    {
+        int a;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+      double b=a*(5.0/18.0);
+        System.out.printf("%.2f",b);
+    }
+}
